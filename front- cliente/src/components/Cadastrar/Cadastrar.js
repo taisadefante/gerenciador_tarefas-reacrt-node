8174d043b63a8ffd531df43dfadd9989c7190b70
tais defante <taisadefante@hotmail.com>
2024-06-25 16:41:57 -0300

    alteração lista

--- front- cliente/src/components/Cadastrar/Cadastrar.js
+++ front- cliente/src/components/Cadastrar/Cadastrar.js
@@ -1,13 +1,44 @@
 import React, { useState } from "react";
-import { Button, Form, Jumbotron, Modal } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Button, Form, Modal } from "react-bootstrap";
+import { Link, Route } from "react-router-dom";
+import Tarefa from "../../models/tarefa.model";
+import Listar from "../Listar/Listar";
+import Style from "./cadastrar.module.css";
 
 function Cadastrar() {
+  const [tarefa, setTarefa] = useState("");
+  const [formValidado, setFormValidado] = useState(false);
+  const [exibirModal, setExibirModal] = useState(false);
+
+  function cadastrar(event) {
+    event.preventDefault();
+    setFormValidado(true);
+    if (event.currentTarget.checkValidity() === true) {
+      //obtem as tarefas
+      const tarefasDb = localStorage["tarefas"];
+      const tarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+
+      //persiste a tarefa
+      tarefas.push(new Tarefa(new Date().getTime(), tarefa, false));
+      localStorage["tarefas"] = JSON.stringify(tarefas);
+      setExibirModal(true);
+    }
+  }
+
+  function handletxtTarefa(event) {
+    setTarefa(event.target.value);
+  }
+
+  function handleFecharModal() {
+    <Route path="/" element={<Listar />} />;
+  }
+
   return (
     <div>
       <h3 className="text-center">Cadastrar</h3>
+
       <div>
-        <Form>
+        <Form validated={formValidado} noValidate onSubmit={cadastrar}>
           <Form.Group>
             <Form.Label>Tarefa</Form.Label>
             <Form.Control
@@ -16,11 +47,14 @@ function Cadastrar() {
               minLength="5"
               maxLength="100"
               required
+              value={tarefa}
+              onChange={handletxtTarefa}
             />
             <Form.Control.Feedback type="invalid">
               A tarefa deve conter ao menos 5 caracteres.
             </Form.Control.Feedback>
           </Form.Group>
+
           <Form.Group className="text-center">
             <Button variant="success" type="submit">
               Cadastrar
@@ -31,13 +65,17 @@ function Cadastrar() {
             </Link>
           </Form.Group>
         </Form>
-        <Modal show={false}>
+        <Modal show={exibirModal} onHide={handleFecharModal}>
           <Modal.Header closeButton>
             <Modal.Title>Sucesso</Modal.Title>
           </Modal.Header>
           <Modal.Body>Tarefa adicionada com sucesso!</Modal.Body>
           <Modal.Footer>
-            <Button variant="success">Continuar</Button>
+            <Button variant="success" onClick={handleFecharModal}>
+              <Link to="/" variant="success" className={Style.link_continuar}>
+                Continuar
+              </Link>
+            </Button>
           </Modal.Footer>
         </Modal>
       </div>
--- front- cliente/src/components/Listar/Listar.js
+++ front- cliente/src/components/Listar/Listar.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Listar() {
+  const [tarefas, setTarefas] = useState([]);
+  const [carregarTarefas, setCarregarTarefas] = useState(true);
+
+  useEffect(() => {
+    function obterTarefas() {
+      const tarefasDb = localStorage["tarefas"];
+      let listarTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+      setTarefas(listarTarefas);
+    }
+    if (carregarTarefas) {
+      obterTarefas();
+      setCarregarTarefas(false);
+    }
+  }, [carregarTarefas]);
+
   return (
-    <div>
-      <h1>Listagem de Tarefas</h1>
-      <Link to="/cadastrar">
-        <Button variant="success" type="submit">
-          Nova Tarefa
-        </Button>
-      </Link>
+    <div className="text-center">
+      <h3>Tarefas a fazer</h3>
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>
+              <Link to="/cadastrar">
+                <Button className="btn btn-success btn-sm" type="submit">
+                  <FontAwesomeIcon icon={faPlus} />
+                  &nbsp; Nova Tarefa
+                </Button>
+              </Link>
+            </th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </Table>
     </div>
   );
 }
